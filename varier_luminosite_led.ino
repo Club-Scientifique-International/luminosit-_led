6915fe169b857a17b81171d5d905e7bf2d79d832
CSI Club Scientifique International <78037173+Elie-bot@users.noreply.github.com>
2022-04-05 19:49:02 +0200

    Add files via upload

---
+++ varier_luminosite_led.ino
@@ -0,0 +1,29 @@
+// Définition des broches et variables utilisées
+int LED = 3;
+// Rapport cyclique
+int x; 
+
+void setup() {
+   pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  x = 0;
+
+  // Allumer progressivement la LED (0 --> 255)
+  while (x <= 255) {
+    analogWrite(LED, x);
+    delay(10);
+    x = x + 1;
+  }
+
+  // Eteindre progressivement la LED (255 --> 0)
+  x = 255;
+  while (x >= 0) {   
+
+    analogWrite(LED, x);
+    delay(10);
+    x = x - 1;
+  }
+  
+}
